0e0c0b727f014aa7811e09fd295639c1b78b76f1
imikram15 <buttikram15@gmail.com>
2024-01-19 12:05:24 +0500

    Assignment Form Completed

--- src/app/forms/form-assignment/form-assignment.component.ts
+++ src/app/forms/form-assignment/form-assignment.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgIfContext } from '@angular/common';
+import { Component, TemplateRef } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, NgForm, Validators, FormArray, Form } from '@angular/forms';
 
 @Component({
@@ -8,113 +9,126 @@ import { FormGroup, FormControl, FormBuilder, NgForm, Validators, FormArray, For
 })
 export class FormAssignmentComponent {
 
-
-  assignmentForm: FormGroup;
-  inputForm: FormGroup;
-  proValues: any = [];
+  forUpdate: boolean = false;
+  totalPrice: number = 0;
+  arrayValue: any = [];
+  formValue: any;
+  rowIndex: any;
   edit: any;
-  rowindex: any;
+  assignmentForm: FormGroup;
+  TotalRow: any;
+  
+
 
   constructor(private fb: FormBuilder) {
-    this.inputForm = this.fb.group({
-      productName: ['', Validators.required],
-      type: ['Product Type', Validators.required],
-      box: ['true'],
-      price: ['', Validators.required]
-    })
     this.assignmentForm = this.fb.group({
-      productRows: this.fb.array([])
+      productRows: this.fb.array([this.additems()])
+    });
+
+  }
+  additems() {
+    return this.fb.group({
+      productName: ['', Validators.required],
+      type: ['', Validators.required],
+      box: ['true'],
+      price: ['', [Validators.required,  Validators.pattern("^[0-9]*$") ]]
     })
   }
 
-  inputadd() {
 
-  }
-  getProductControls(): FormGroup[] {
-    return (this.assignmentForm.get('productRows') as FormArray)?.controls as FormGroup[];
+  addProduct() {
+    const product = <FormArray>this.assignmentForm.controls['productRows'];
+    product.push(this.additems());
   }
 
   get productRows() {
-    // console.log(this.assignmentForm.get('productRows')?.value);
-
     return this.assignmentForm.controls['productRows'] as FormArray
   }
 
-  saveData() {
-    if (this.rowindex == null) {
 
-      (this.assignmentForm.get("productRows") as FormArray).push(this.fb.group({
-        productName: [this.inputForm.value.productName],
-        type: [this.inputForm.value.type],
-        box: [this.inputForm.value.box],
-        price: [this.inputForm.value.price]
-      }))
-      this.inputForm.get('productName')?.reset()
-      this.inputForm.get('price')?.reset()
-      this.inputForm.get('type')?.reset()
+  saveData() {
+    if (this.rowIndex == null) {
+      this.formValue = this.assignmentForm.getRawValue();
+      this.arrayValue.push(...this.formValue.productRows.map((row: any) => ({
+        productName: row.productName,
+        type: row.type,
+        box: row.box,
+        price: row.price
+      })));
+      this.assignmentForm.reset();
+      //for Deleting the remaining input Rows
+      const newcontrol = <FormArray>this.assignmentForm.controls['productRows'];      
+      if(newcontrol.value.length > 1){
+       const forDeletion = newcontrol.value.length - 1;
+         for (let i = 0; i <= forDeletion; i++) {
+          newcontrol.removeAt(1);
+         }
+      }
+      // for Calculating the Price.
+      this.totalPrice = 0;
+    for (let i = 0; i < this.arrayValue.length; i++) {
+      const tprice = this.arrayValue[i].price || 0;
+      this.totalPrice += parseInt(tprice);
+    }
     }
 
     else {
-      const control3 = (this.assignmentForm.get('productRows') as FormArray).at(this.rowindex) as FormGroup;
+      const control3 = (this.assignmentForm.get('productRows') as FormArray).at(0) as FormGroup;
 
-      control3.setValue({
-        productName: this.inputForm.get('productName')?.value,
-        type: this.inputForm.get('type')?.value,
-        box: this.inputForm.get('box')?.value,
-        price: this.inputForm.get('price')?.value,
-      });
-      this.inputForm.get('productName')?.reset()
-      this.inputForm.get('price')?.reset()
-      this.inputForm.get('type')?.reset()
+      this.arrayValue[this.rowIndex] = {
+        productName: control3.get('productName')?.value,
+        type: control3.get('type')?.value,
+        box: control3.get('box')?.value,
+        price: control3.get('price')?.value,
+      };
+      this.rowIndex = null;
+      this.assignmentForm.reset();
+  
     }
 
-    //   if(this.rowindex != null){
-    //     const control3 = (this.assignmentForm.get('productRows') as FormArray).at(this.rowindex);
-    //  control3.value.productName = this.inputForm.get('productName')?.value ;
-    // control3.value.type = this.inputForm.get('type')?.value 
-    // control3.value.box = this.inputForm.get('box')?.value 
-    // control3.value.price = this.inputForm.get('price')?.value 
-    //   }
-
-
+    this.forUpdate = false;
   }
 
 
 
-  editData(index: any) {
-    this.rowindex = index;
-    this.edit = this.assignmentForm.value.productRows[this.rowindex];
-    this.inputForm.setValue({
+  editData(editIndex: any) {
+    this.rowIndex = editIndex;
+    this.edit = this.arrayValue[this.rowIndex];
+
+    const productRows2 = this.assignmentForm.get('productRows') as FormArray;
+    const editedProduct = productRows2.at(0) as FormGroup;
+      
+    editedProduct.patchValue({
       productName: this.edit?.productName,
       type: this.edit?.type,
       box: this.edit?.box,
       price: this.edit?.price,
     });
-
-  }
+  
+  this.forUpdate = true;
+}
 
   deleteProduct(productindex: number) {
     let text = "Are You Sure. you wanr to Delete it?";
-    if (confirm(text) == true) {
-      this.productRows.removeAt(productindex);
+
+    const control = <FormArray>this.assignmentForm.controls['productRows'];
+    if (control != null) {
+      this.TotalRow = control.value.length;
     }
+    if (this.TotalRow > 1) {
+      if (confirm(text) == true) {
+        control.removeAt(productindex);
+      }
+    } else {
+      alert('One Input Field is Mandatory.');
+      return false;
     }
+    return true;
+  }
+
+  delTableRow(index: number) {
+    this.arrayValue.splice(index, 1);
+  }
 
 }
 
-
-
-// additems() {
-//   return this.fb.group({
-//     productName: ['', Validators.required],
-//     type: ['Select the Product Type', Validators.required],
-//     box: ['true'],
-//     price: ['', Validators.required]
-//   })
-// }
-
-// addProduct() {
-//   const products = <FormArray>this.assignmentForm.controls['productRows'];
-//   products.push(this.additems());
-//   // console.log(this.productRows);
-// }
